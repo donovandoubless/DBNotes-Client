4e6a6f491c4ed580f149dc545c6823c3aed65789
Donovan Bailey <donovan.doubless@gmail.com>
2021-04-30 19:04:48 +0800

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
-	app.use(proxy("/auth", { target: "https://mydb-notes.herokuapp.com/" }));
+	app.use(proxy("/auth", { target: "https://mydb-notes.herokuapp.com" }));
 };
